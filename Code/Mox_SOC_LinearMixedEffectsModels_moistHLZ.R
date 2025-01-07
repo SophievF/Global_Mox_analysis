c9925baaa1aa1f5c98779031137da281281ba086
Sophie von Fromm <sfromm@dartmouth.edu>
2025-01-07 16:16:53 -0500

    update figure numbers

--- Code/Mox_SOC_LinearMixedEffectsModels_moistHLZ.R
+++ Code/Mox_SOC_LinearMixedEffectsModels_moistHLZ.R
@@ -9,11 +9,13 @@ library(scales)
 library(nlme)
 library(MuMIn)
 library(emmeans)
+library(sp)
+library(gstat)
 
 #### Linear mixed-effects models: moisture groups
 
 ## Load and prepare data
-all_data <- read_csv("./Data/Database_HLZ_grp_2024-05-27.csv") %>% 
+all_data <- read_csv("./Data/Database_HLZ_grp_2024-09-09.csv") %>% 
   mutate(al_fe_ox = al_ox + (1/2*fe_ox)) %>% 
   filter(hzn_bot <= 200) %>% 
   mutate(depth_cat = cut(hzn_mid, breaks = c(0, 20, 50, 100, 200), 
@@ -69,22 +71,22 @@ hlz_color_moist <- c("#e5e5b7", "#a1dab4", "#41b6c4", "#2c7fb8", "#253494")
 all_data_lm_moist <- all_data %>% 
   drop_na(al_ox, fe_ox) %>% 
   # set 0s to smallest measured value/2 = 0.005
-  dplyr::mutate(al_ox = al_ox + 0.005,
-                carbon = carbon + 0.005,
-                fe_ox = fe_ox + 0.005,
-                al_fe_ox = al_fe_ox + 0.005) %>% 
-  mutate_if(is.integer, as.numeric) %>% 
-  # mutate_at(c("carbon", "al_ox", "hzn_mid"), scale) %>% 
-  group_by(HLZ_moist) %>% 
-  mutate_at(c("carbon", "al_ox", "hzn_mid", "fe_ox", "al_fe_ox"), 
-            ~predict(bestNormalize::boxcox(.x))) %>% 
-  ungroup()
+  dplyr::mutate(al_ox = log(al_ox + 0.005),
+                carbon = log(carbon + 0.005),
+                fe_ox = log(fe_ox + 0.005),
+                al_fe_ox = log(al_fe_ox + 0.005)) 
 
-skimr::skim_without_charts(all_data_lm_moist)
+# skimr::skim_without_charts(all_data_lm_moist)
+
+all_data_lm_moist %>% 
+  group_by(depth_cat, HLZ_moist) %>% 
+  ggplot(aes(y = al_fe_ox, x = HLZ_moist)) +
+  geom_boxplot() +
+  facet_wrap(~depth_cat)
 
 ### Fit model
 ## Alox + 1/2 Feox
-# Random slope for depth
+# Final model
 lmm_al_fe_depth_moist <- lme(carbon ~ al_fe_ox*HLZ_moist + al_fe_ox*depth_cat + al_fe_ox*HLZ_moist*depth_cat, 
                              random = ~hzn_mid|ID,
                              data = all_data_lm_moist)
@@ -114,10 +116,32 @@ R_Final <- residuals(lmm_al_fe_depth_moist)
 hist(R_Final)
 boxplot(R_Final ~ all_data_lm_moist$HLZ_moist)
 boxplot(R_Final ~ all_data_lm_moist$depth_cat)
-plot(R_Final ~ all_data_lm_moist$hzn_mid)
 plot(R_Final ~ all_data_lm_moist$al_fe_ox)
 
+# Partial residual plots
+mox_c <- fixef(lmm_al_fe_depth_moist)[2] #predictor coefficient
+mox_pr <- R_Final + mox_c * all_data_lm_moist$al_fe_ox
+
+{scatter.smooth(all_data_lm_moist$al_fe_ox, mox_pr,
+                lpars = list(col = "green", lwd = 3, lty = 3)) #residual loess
+  abline(lm(mox_c*all_data_lm_moist$al_fe_ox ~ all_data_lm_moist$al_fe_ox), col = "red")}
+
 #--> looks all good: similar variance across groups and generally homogeneous
+
+# Check for spatial correlation
+resid_norm <- resid(lmm_al_fe_depth_moist, type = "normalized")
+data_moist <- data.frame(resid_norm, all_data_lm_moist$Longitude, 
+                         all_data_lm_moist$Latitude)
+coordinates(data_moist) <- c("all_data_lm_moist.Longitude",
+                                    "all_data_lm_moist.Latitude")
+bubble(data_moist, "resid_norm", col = c("black", "grey"),
+       main = "Normalised residuals",
+       xlab = "X-coordinates", ylab = "Y-coordinates")
+
+Vario1 <- variogram(resid_norm ~ 1, data_moist)
+plot(Vario1)
+
+#--> Looks all good, no increase in semivariacen with distance
 
 ## Alox
 # Random slope for depth
@@ -150,8 +174,28 @@ R_Final <- residuals(lmm_al_depth_moist)
 hist(R_Final)
 boxplot(R_Final ~ all_data_lm_moist$HLZ_moist)
 boxplot(R_Final ~ all_data_lm_moist$depth_cat)
-plot(R_Final ~ all_data_lm_moist$hzn_mid)
 plot(R_Final ~ all_data_lm_moist$al_ox)
+
+# Partial residual plots
+mox_c <- fixef(lmm_al_depth_moist)[2] #predictor coefficient
+mox_pr <- R_Final + mox_c * all_data_lm_moist$al_ox
+
+{scatter.smooth(all_data_lm_moist$al_ox, mox_pr,
+                lpars = list(col = "green", lwd = 3, lty = 3)) #residual loess
+  abline(lm(mox_c*all_data_lm_moist$al_ox ~ all_data_lm_moist$al_ox), col = "red")}
+
+# Check for spatial correlation
+resid_norm <- resid(lmm_al_depth_moist, type = "normalized")
+data_moist <- data.frame(resid_norm, all_data_lm_moist$Longitude, 
+                         all_data_lm_moist$Latitude)
+coordinates(data_moist) <- c("all_data_lm_moist.Longitude",
+                             "all_data_lm_moist.Latitude")
+bubble(data_moist, "resid_norm", col = c("black", "grey"),
+       main = "Normalised residuals",
+       xlab = "X-coordinates", ylab = "Y-coordinates")
+
+Vario2 <- variogram(resid_norm ~ 1, data_moist)
+plot(Vario2)
 
 ## Feox
 # Random slope for depth
@@ -184,20 +228,39 @@ R_Final <- residuals(lmm_fe_depth_moist, type = "response", scaled = TRUE) #type
 hist(R_Final)
 boxplot(R_Final ~ all_data_lm_moist$HLZ_moist)
 boxplot(R_Final ~ all_data_lm_moist$depth_cat)
-plot(R_Final ~ all_data_lm_moist$hzn_mid)
 plot(R_Final ~ all_data_lm_moist$fe_ox)
 
+mox_c <- fixef(lmm_fe_depth_moist)[2] #predictor coefficient
+mox_pr <- R_Final + mox_c * all_data_lm_moist$fe_ox
+
+{scatter.smooth(all_data_lm_moist$fe_ox, mox_pr,
+                lpars = list(col = "green", lwd = 3, lty = 3)) #residual loess
+  abline(lm(mox_c*all_data_lm_moist$fe_ox ~ all_data_lm_moist$fe_ox), col = "red")}
+
+# Check for spatial correlation
+resid_norm <- resid(lmm_fe_depth_moist, type = "normalized")
+data_moist <- data.frame(resid_norm, all_data_lm_moist$Longitude, 
+                         all_data_lm_moist$Latitude)
+coordinates(data_moist) <- c("all_data_lm_moist.Longitude",
+                             "all_data_lm_moist.Latitude")
+bubble(data_moist, "resid_norm", col = c("black", "grey"),
+       main = "Normalised residuals",
+       xlab = "X-coordinates", ylab = "Y-coordinates")
+
+Vario3 <- variogram(resid_norm ~ 1, data_moist)
+plot(Vario3)
+
 ## Compare all three models (based on method = Maximum Likelihood)
-# lmm_al_fe_depth_moist_ML <- lme(carbon ~ al_fe_ox*HLZ_moist + al_fe_ox*depth_cat + al_fe_ox*HLZ_moist*depth_cat, 
+# lmm_al_fe_depth_moist_ML <- lme(carbon ~ al_fe_ox*HLZ_moist + al_fe_ox*depth_cat + al_fe_ox*HLZ_moist*depth_cat,
 #                                 random = ~hzn_mid|ID, method = "ML",
 #                                 data = all_data_lm_moist)
-# lmm_al_depth_moist_ML <- lme(carbon ~ al_ox*HLZ_moist + al_ox*depth_cat + al_ox*HLZ_moist*depth_cat, 
+# lmm_al_depth_moist_ML <- lme(carbon ~ al_ox*HLZ_moist + al_ox*depth_cat + al_ox*HLZ_moist*depth_cat,
 #                              random = ~hzn_mid|ID, method = "ML",
 #                              data = all_data_lm_moist)
-# lmm_fe_depth_moist_ML <- lme(carbon ~ fe_ox*HLZ_moist + fe_ox*depth_cat + fe_ox*HLZ_moist*depth_cat, 
+# lmm_fe_depth_moist_ML <- lme(carbon ~ fe_ox*HLZ_moist + fe_ox*depth_cat + fe_ox*HLZ_moist*depth_cat,
 #                              random = ~hzn_mid|ID, method = "ML",
 #                              data = all_data_lm_moist)
-# AIC(lmm_al_fe_depth_moist_ML, lmm_al_depth_moist_ML, lmm_fe_depth_moist_ML)
+# anova(lmm_al_fe_depth_moist_ML, lmm_al_depth_moist_ML, lmm_fe_depth_moist_ML)
 
 ### Post-Hoc analysis
 ## Alox + 1/2 Feox
@@ -213,6 +276,9 @@ em_al_fe_moist$emtrends
 em_al_fe_moist$contrasts
 #significant differences between slopes
 
+as.data.frame(em_al_fe_moist$contrasts) %>% 
+  filter(p.value <= 0.05) 
+
 emip_al_fe_moist <- emmip(lmm_al_fe_depth_moist, HLZ_moist ~ al_fe_ox | depth_cat, cov.reduce = range, 
                           plotit = FALSE, CIs = TRUE)
 
@@ -221,7 +287,7 @@ emip_al_fe_moist$HLZ_moist <- as.ordered(emip_al_fe_moist$HLZ_moist)
 emip_al_fe_data_moist <- emip_al_fe_moist %>% 
   as.data.frame() %>% 
   left_join(all_data %>% 
-              distinct(DESC_filled, ZONE_filled, HLZ_moist), 
+              distinct(HLZ_moist), 
             multiple = "all")
 
 emip_al_fe_data_moist$HLZ_moist <- factor(emip_al_fe_data_moist$HLZ_moist, 
@@ -239,14 +305,33 @@ emip_al_fe_moist %>%
   facet_wrap(~depth_cat) +
   scale_color_manual("Moisture group", values = hlz_color_moist) +
   scale_fill_manual("Moisture group", values = hlz_color_moist) +
-  scale_x_continuous(expression(paste("Relative concentration in M"[ox], " within each moisture group")), 
-                     expand = c(0,0), labels = c("", "low", "", "", "high", "")) +
-  scale_y_continuous("Relative predicted SOC content", 
-                     expand = c(0,0), limits = c(-4.5,4), labels = c("low", "", "", "", "high"))
+  scale_x_continuous(expression(paste("Log-scaled concentration in M"[ox])), 
+                     expand = c(0,0)) +
+  scale_y_continuous("Predicted log-scaled SOC content", 
+                     expand = c(0,0), limits = c(-5,3))
 ggsave(file = paste0("./Output/Figure3_", 
                      Sys.Date(), ".jpeg"), width = 14, height = 7)
 
-
+emip_al_fe_moist %>%   
+  ggplot(aes(x = al_fe_ox, y = yvar)) +
+  geom_point(data = all_data_lm_moist, aes(x = al_fe_ox, y = carbon, 
+                                           color = HLZ_moist),
+             shape = 21) +
+  geom_ribbon(aes(ymin = LCL, ymax = UCL, fill = HLZ_moist), alpha = 0.25) +
+  geom_line(aes(color = HLZ_moist), linewidth = 1.5) +
+  theme_bw(base_size = 18) +
+  theme(axis.text = element_text(color = "black"),
+        axis.ticks = element_blank(),
+        legend.position = "none") +
+  facet_wrap(vars(depth_cat, HLZ_moist)) +
+  scale_color_manual("Moisture group", values = hlz_color_moist) +
+  scale_fill_manual("Moisture group", values = hlz_color_moist) +
+  scale_x_continuous(expression(paste("Log-scaled concentration in M"[ox])),
+                     breaks = seq(-5,5,2.5)) +
+  scale_y_continuous("Predicted log-scaled SOC content",
+                     breaks = seq(-5,2.5,2.5))
+ggsave(file = paste0("./Output/FigureA8_", 
+                     Sys.Date(), ".jpeg"), width = 12, height = 10)
 
 # Prepare data for summary table
 all_data_mox_summary <- all_data %>% 
@@ -266,7 +351,6 @@ mox_n_samples <- all_data %>%
   drop_na(al_fe_ox) %>%
   group_by(HLZ_moist, depth_cat) %>% 
   summarise(n = n())
-
 
 all_data_mox_summary$HLZ_moist <- factor(all_data_mox_summary$HLZ_moist, 
                                          levels = c("arid", "semiarid", "subhumid", 
@@ -293,7 +377,6 @@ posthoc_mox_summary <- em_al_fe_moist_slopes %>%
 write_csv(x = posthoc_mox_summary, file = paste0("./Output/Table1_", 
                                               Sys.Date(), ".csv"))
 
-
 ## Alox
 summary(lmm_al_depth_moist)
 anova(lmm_al_depth_moist)
@@ -314,7 +397,7 @@ emip_al_moist$HLZ_moist <- as.ordered(emip_al_moist$HLZ_moist)
 emip_al_data_moist <- emip_al_moist %>% 
   as.data.frame() %>% 
   left_join(all_data %>% 
-              distinct(DESC_filled, ZONE_filled, HLZ_moist), 
+              distinct(HLZ_moist), 
               multiple = "all")
 
 emip_al_data_moist$HLZ_moist <- factor(emip_al_data_moist$HLZ_moist, 
@@ -331,11 +414,11 @@ emip_al_moist %>%
   facet_wrap(~depth_cat) +
   scale_color_manual("Moisture group", values = hlz_color_moist) +
   scale_fill_manual("Moisture group", values = hlz_color_moist) +
-  scale_x_continuous(expression(paste("Relative concentration in Al"[ox], " within each moisture group")), 
-                     expand = c(0,0), labels = c("low", "", "", "", "", "high")) +
-  scale_y_continuous("Relative predicted SOC content", 
-                     expand = c(0,0), limits = c(-4.5,4), labels = c("low", "", "", "", "high"))
-ggsave(file = paste0("./Output/FigureA8_", 
+  scale_x_continuous(expression(paste("Log-scaled concentration in Al"[ox])), 
+                     expand = c(0,0)) +
+  scale_y_continuous("Predicted log-scaled SOC content", 
+                     expand = c(0,0), limits = c(-5,3))
+ggsave(file = paste0("./Output/FigureA9_", 
                      Sys.Date(), ".jpeg"), width = 14, height = 7)
 
 ## Feox
@@ -358,11 +441,11 @@ emip_fe_moist$HLZ_moist <- as.ordered(emip_fe_moist$HLZ_moist)
 emip_fe_data_moist <- emip_fe_moist %>% 
   as.data.frame() %>% 
   left_join(all_data %>% 
-              distinct(DESC_filled, ZONE_filled, HLZ_moist), 
+              distinct(HLZ_moist), 
             multiple = "all")
 
 emip_fe_data_moist$HLZ_moist <- factor(emip_fe_data_moist$HLZ_moist, 
-                                       levels = unique(emip_fe_data_moist$HLZ_moist[order(emip_fe_data_moist$ZONE_filled)]), 
+                                       levels = c("arid", "semiarid", "subhumid", "humid", "perhumid"),, 
                                        ordered = TRUE)
 
 emip_fe_moist %>%   
@@ -375,9 +458,9 @@ emip_fe_moist %>%
   facet_wrap(~depth_cat) +
   scale_color_manual("Moisture group", values = hlz_color_moist) +
   scale_fill_manual("Moisture group", values = hlz_color_moist) +
-  scale_x_continuous(expression(paste("Relative concentration in Fe"[ox], " within each moisture group")), 
-                     expand = c(0,0), labels = c("", "low", "", "high", "")) +
-  scale_y_continuous("Relative predicted SOC content", 
-                     expand = c(0,0), limits = c(-2,3), labels = c("low", "", "", "", "", "high"))
-ggsave(file = paste0("./Output/FigureA9_", 
+  scale_x_continuous(expression(paste("Log-scaled concentration in Fe"[ox])), 
+                     expand = c(0,0)) +
+  scale_y_continuous("Predicted log-scaled SOC content", 
+                     expand = c(0,0), limits = c(-5,3))
+ggsave(file = paste0("./Output/FigureA10_", 
                      Sys.Date(), ".jpeg"), width = 14, height = 7)
